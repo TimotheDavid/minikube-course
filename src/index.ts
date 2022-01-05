7408f00f636083494ce0fe3fadc0536c25a26412
david_t <david_t@etna-alternance.net>
2022-01-05 13:00:19 +0100

    add all and fix eslint

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import router from './routes/router'
 const app = express()
 
-const PORT = 4242;
+const PORT = 80;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
